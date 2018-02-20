7ee0a2da32d93d944392f61a690aa7c0a0abdb05
Hong <hongnoh12@gmail.com>
2018-02-19 22:15:27 -0500

    changes to convert strings to numbers
    convert strings to numbers and remove unnecessary text froms scrape.

--- scrape/scrape.R
+++ scrape/scrape.R
@@ -18,15 +18,15 @@ box_data = html_nodes(webpage_HTML,".lister-item.mode-advanced")
 # runtime data:
 runtime_tmp = box_data %>% html_nodes(".text-muted") %>% html_nodes(".runtime") %>% html_text()
 runtime_data = vector()
-for (i in 0:length(runtime_tmp)) {
-  runtime_data[i] = substr(tmp[i],1,3)
+for (i in 1:length(runtime_tmp)) {
+  runtime_data[i] = as.numeric(substr(runtime_tmp[i],1,3))
 }
 
 # certificate data:
 certificate_data = box_data %>% html_nodes(".text-muted") %>% html_nodes(".certificate") %>% html_text()
 
 # genre data:
-gen_data = vector()
+genre_data = vector()
 genre_tmp = box_data %>% html_nodes(".text-muted") %>% html_nodes(".genre") %>% html_text()
 for (i in 0:length(genre_tmp)){
   genre_data[i] = trimws(genre_tmp[i])
@@ -39,4 +39,8 @@ votes_data = box_data %>% html_nodes(".sort-num_votes-visible") %>% html_text()
 star_data = box_data %>% html_nodes(".ratings-bar") %>% html_nodes("strong") %>% html_text()
 
 # metascore data: (incmplete)
-metascore_data = box_data %>% html_nodes(".ratings-bar") %>% html_nodes(".inline-block.ratings-metascore") %>% html_text()
+metascore_tmp = box_data %>% html_nodes(".ratings-bar") %>% html_nodes(".inline-block.ratings-metascore") %>% html_text()
+metascore_data = vector()
+for (i in 1:length(metascore_tmp)) {
+  metascore_data[i] = as.numeric(trimws(strsplit(metascore_tmp[i],"\n")[[1]][2]))
+}
